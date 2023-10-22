7d2ad8905ad3cd312f8c1c3eed4015f4ef90fb50
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2023-10-22 13:05:36 +0800

    Refactor Tabs

--- src/components/compose/PopoverContent.tsx
+++ src/components/compose/PopoverContent.tsx
@@ -1,10 +1,5 @@
-import { JSX } from 'preact'
-import { Tab as BaseTab, TabOwnerState, TabProps } from '@mui/base/Tab'
-import { TabsList as BaseTabsList, TabsListProps } from '@mui/base/TabsList'
-import { TabPanel as BaseTabPanel, TabPanelProps } from '@mui/base/TabPanel'
-import { Tabs } from '@mui/base/Tabs'
+import { ComponentChildren, JSX } from 'preact'
 import clsx from 'clsx'
-import { forwardRef } from 'preact/compat'
 import { useState } from 'preact/hooks'
 import { CharPanel } from './popover/CharPanel'
 import { UploadButton } from './popover/UploadButton'
@@ -15,6 +10,7 @@ import { StickerPanel } from './popover/StickerPanel'
 export function PopoverContent (): JSX.Element {
   const [stickerDisabled, setStickerDisabled] = useState(true)
   const context = useAppContext()
+  const [selected, setSelected] = useState(1)
   const shownCharacters = context.shownCharacters
   useSignalEffect(() => {
     const character = shownCharacters.value[context.lastIndex.value]
@@ -22,79 +18,63 @@ export function PopoverContent (): JSX.Element {
   })
 
   return (
-    <Tabs className='flex flex-col-reverse max-w-sm gap-1' defaultValue={1}>
-      <TabsList className='flex-row'>
-        <Tab title='Characters' className='w-8 h-8' value={1}><span class='w-6 h-6 icon-[solar--user-rounded-bold-duotone]' /></Tab>
-        <Tab disabled={stickerDisabled} title='Stickers' className='w-8 h-8' value={2}><span class='w-6 h-6 icon-[solar--sticker-smile-circle-bold-duotone]' /></Tab>
+    <div className='flex flex-col-reverse max-w-sm gap-1'>
+      <div className='flex flex-row' role='tablist'>
+        <Tab title='Characters' className='w-8 h-8' selected={selected === 1} onClick={() => setSelected(1)}>
+          <span aria-hidden class='w-6 h-6 icon-[solar--user-rounded-bold-duotone]' />
+        </Tab>
+        <Tab disabled={stickerDisabled} title='Stickers' selected={selected === 2} className='w-8 h-8' onClick={() => setSelected(2)}>
+          <span aria-hidden class='w-6 h-6 icon-[solar--sticker-smile-circle-bold-duotone]' />
+        </Tab>
         <div class='grow' />
         <UploadButton />
-      </TabsList>
-      <TabPanel value={1}><CharPanel /></TabPanel>
-      <TabPanel value={2}><StickerPanel /></TabPanel>
-    </Tabs>
+      </div>
+      <TabPanel shown={selected === 1}><CharPanel /></TabPanel>
+      <TabPanel shown={selected === 2}><StickerPanel /></TabPanel>
+    </div>
   )
 }
 
-const resolveSlotProps = (fn: any, args: any): any =>
-  typeof fn === 'function' ? fn(args) : fn
+function Tab (props: {
+  className: string
+  selected: boolean
+  title: string
+  children: ComponentChildren
+  onClick: any
+  disabled?: boolean
+}): JSX.Element {
+  const { className, selected, title, disabled = false, ...other } = props
 
-const TabsList = forwardRef<HTMLDivElement, TabsListProps>((props, ref) => {
-  const { className, ...other } = props
   return (
-    <BaseTabsList
-      ref={ref}
+    <button
+      role='tab'
+      {...other}
       className={clsx(
-        'flex',
+        `rounded-xl transition duration-200 bg-transparent hover:bg-gray-100 dark:hover:bg-gray-900 ${
+          props.selected
+            ? 'text-orange-600'
+            : 'text-black/75 dark:text-white/75'
+        } disabled:cursor-not-allowed disabled:opacity-25 cursor-pointer
+        flex justify-center items-center`,
         className
       )}
-      {...other}
+      disabled={disabled}
+      title={title}
     />
   )
-})
+}
 
-const Tab = forwardRef<HTMLButtonElement, TabProps>((props, ref) => {
+function TabPanel (props: { className?: string, shown: boolean, children: ComponentChildren }): JSX.Element {
+  const { className, shown, ...other } = props
   return (
-    <BaseTab
-      ref={ref}
-      {...props}
-      slotProps={{
-        ...props.slotProps,
-        root: (ownerState: TabOwnerState) => {
-          const resolvedSlotProps = resolveSlotProps(
-            props.slotProps?.root,
-            ownerState
-          )
-          return {
-            ...resolvedSlotProps,
-            className: clsx(
-              `rounded-xl transition duration-200 bg-transparent hover:bg-gray-100 dark:hover:bg-gray-900 ${
-                ownerState.selected
-                  ? 'text-orange-600'
-                  : 'text-black/75 dark:text-white/75'
-              } ${
-                ownerState.disabled
-                  ? 'cursor-not-allowed opacity-25'
-                  : 'cursor-pointer'
-              } flex justify-center items-center`,
-              resolvedSlotProps?.className
-            )
-          }
-        }
-      }}
-    />
-  )
-})
-
-const TabPanel = forwardRef<HTMLDivElement, TabPanelProps>((props, ref) => {
-  const { className, ...other } = props
-  return (
-    <BaseTabPanel
-      ref={ref}
+    <div
+      role='tabpanel'
       className={clsx(
         'p-1 border border-solid border-slate-200 dark:border-slate-700 rounded-lg',
+        { hidden: !shown },
         className
       )}
       {...other}
     />
   )
-})
+}
